4c4415f811f0c7ef902e8d0c89177d37afe364b6
Kristofer Muñoz <kristofer.munoz@mail.udp.cl>
2025-03-25 14:19:42 -0300

    Add files via upload

---
+++ Lab 1 Eda/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class ASCII() {
+    private final int dig[];
+    private final char letter[];
+    private final char letterM[];
+
+    public ASCII(int []dig, char []letter, char []letterM) {
+        this.dig = dig;
+        this.letter = letter;
+        this.letterM = letterM;
+
+    }
+    void RellenarDig()
+    {
+        for(int i=0;i<=9;i++)
+        {
+            dig[i]=i;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+public class BigViginere() {
+    private int[] key;
+    public BigViginere() {
+        char[][] alphabet = new char[64][64];
+        int[] key = new int[10];
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+
+
+ }
+}
